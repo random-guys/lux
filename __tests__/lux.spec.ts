b38e4963576a7811f4ec934144f303dfbc83c1d8
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-07-13 14:53:47 +0100

    test: Finally we have tests for lux

---
+++ __tests__/lux.spec.ts
@@ -0,0 +1,23 @@
+import { UnsafeSyncService } from "../src"
+
+const endpoint = 'https://pass.sterling.ng/SxSService/OTPCentralService.asmx?WSDL'
+const namespace = ['OTPCentralService', 'OTPCentralServiceSoap']
+const payload = {
+  username: 'fakeUser',
+  hashkey: '121212121212121211121',
+  otp: '999999'
+}
+
+describe('Unsafe Async Ops', () => {
+  const service = new UnsafeSyncService(endpoint)
+
+  it('should describe the world', async () => {
+    const description = await service.getDescription()
+    expect(description).toBeTruthy()
+  })
+
+  it('should describe the world', async () => {
+    const result = await service.call(payload, ...namespace, 'OtpValidation')
+    expect(result).toBeTruthy()
+  })
+})
