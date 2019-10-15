b15e395d7ce7c6439642e0d7b95ea9bd6971321f
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-10-15 15:31:58 +0100

    feat: Added logging to connection processing
    Also reduced the possibility of a failed transaction killing a full
    process

--- src/lib/service.ts
+++ src/lib/service.ts
@@ -1,97 +1,100 @@
-import { Client } from "soap";
-import { asyncMethod, parseFormatted, parseEmbedded } from "./method";
-import { AsyncMethod, MethodResult } from "./types";
-import { unsafeClient, unsafeRun } from "./unsafe";
-
-
+import { Client } from 'soap';
+import { asyncMethod, parseFormatted, parseEmbedded } from './method';
+import { AsyncMethod, MethodResult } from './types';
+import { unsafeClient, unsafeRun } from './unsafe';
+import Logger = require('bunyan');
 
 export class SoapService {
-  protected readonly methods = {}
-  protected client: Client
+  protected readonly methods = {};
+  protected client: Client;
 
   /**
    * Proxy to `Client.describe`
    */
   getDescription(): Promise<any> {
-    return this.client.describe()
+    return this.client.describe();
   }
 
   call(arg: {}, ...path: string[]): Promise<MethodResult> {
-    let method = this.getMethod(...path)
-    return method(arg)
+    let method = this.getMethod(...path);
+    return method(arg);
   }
 
   callEmbedded(arg: {}, ...path: string[]): Promise<string> {
-    let method = this.getMethod(...path)
-    return method(arg).then(parseEmbedded(path))
+    let method = this.getMethod(...path);
+    return method(arg).then(parseEmbedded(path));
   }
 
   callFormatted(arg: {}, ...path: string[]): Promise<string> {
-    let method = this.getMethod(...path)
-    return method(arg).then(parseFormatted(path))
+    let method = this.getMethod(...path);
+    return method(arg).then(parseFormatted(path));
   }
 
   protected getMethod(...path: string[]): AsyncMethod {
-    let key = path.join('.')
+    let key = path.join('.');
     // cache method for later use
     if (!this.methods[key]) {
-      this.methods[key] = asyncMethod(this.client, ...path)
+      this.methods[key] = asyncMethod(this.client, ...path);
     }
-    return this.methods[key]
+    return this.methods[key];
   }
 }
 
 export class UnsafeSyncService extends SoapService {
-  private queue: Promise<void>
-  constructor(url: string) {
-    super()
+  private queue: Promise<void>;
+  constructor(url: string, logger: Logger) {
+    super();
 
     // connect without security
     this.queue = unsafeRun(async () => {
-      return await unsafeClient(url).then((cl) => {
-        this.client = cl
-      })
-    })
+      return await unsafeClient(url).then(
+        cl => {
+          this.client = cl;
+          logger.info(`Successfully connect to ${url}`);
+        },
+        err => logger.error({ err })
+      );
+    });
   }
 
   private async assertClient() {
     // wait for connection function
-    await this.queue
+    await this.queue;
 
     // make sure client actually
     if (!this.client) {
-      throw new Error('Client not connected')
+      throw new Error('Client not connected');
     }
   }
 
   async getDescription() {
     // make sure client is initialized
-    await this.assertClient()
+    await this.assertClient();
 
     // defer to parent class
     return unsafeRun(async () => {
-      return super.getDescription()
-    })
+      return super.getDescription();
+    });
   }
 
   protected getMethod(...path: string[]): AsyncMethod {
     // wrap in unsafeRun
     return async (payload: any) => {
       // make sure client is initialized
-      await this.assertClient()
+      await this.assertClient();
       // use existing method definition
-      const method = super.getMethod(...path)
+      const method = super.getMethod(...path);
 
       return unsafeRun(async () => {
-        return method(payload)
-      })
-    }
+        return method(payload);
+      });
+    };
   }
 }
 
 export class AsyncService extends SoapService {
   constructor(client: Client) {
-    super()
-    this.client = client
+    super();
+    this.client = client;
   }
 }
