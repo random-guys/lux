6bc814bf6ad69b7a4ea404101a565926f9545c11
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-10-20 03:19:52 +0100

    fix: callEmbedded now uses generic return types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -5,5 +5,5 @@ export type AsyncMethod = (payload: any) => Promise<any>;
 export type MethodMaker = (client: Client, ...path: string[]) => AsyncMethod;
 
 export interface MethodResult {
-  [key: string]: string;
+  [key: string]: any;
 }
